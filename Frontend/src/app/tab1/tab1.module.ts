521cc217e937c6cac3a64a72a479be57b64d5439
Stroescu Marian-Calin <cali_stroescu@yahoo.com>
2021-09-02 21:42:33 +0300

    First commit

---
+++ Frontend/src/app/tab1/tab1.module.ts
@@ -0,0 +1,22 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Tab1Page } from './tab1.page';
+import { HttpClientModule } from '@angular/common/http';
+
+import { Tab1PageRoutingModule } from './tab1-routing.module';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    Tab1PageRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  declarations: [Tab1Page],
+  providers: [FormBuilder]
+})
+export class Tab1PageModule {}
---
+++ Frontend/src/app/tab1/tab1.page.ts
@@ -0,0 +1,75 @@
+import { Endpoints } from './endpoints';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit{
+
+  submitForm: FormGroup = new FormGroup({
+    text: new FormControl('', Validators.required)
+  }, {updateOn: 'blur'});
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
+
+  movies;
+  reviewSubmitted = [];
+  feedbackLogged = [];
+  predictedSentiments = [];
+  randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+
+  constructor(
+    private http: HttpClient,
+    fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.http.get(Endpoints.API_GET_MOVIES).subscribe(res => {
+      this.movies = res;
+      this.movies.forEach(movie => {
+        this.reviewSubmitted.push(false);
+        this.feedbackLogged.push(false);
+        this.predictedSentiments.push('');
+      });
+    });
+  }
+
+  getMovieTitle(movie): string {
+    return movie.toString().slice(0, -4);
+  }
+
+  setRandomColor() {
+    this.randomColor =  '#'+Math.floor(Math.random()*16777215).toString(16);
+  }
+
+  onSubmit(data, index): void {
+    if(this.submitForm.valid) {
+      this.http.post(Endpoints.API_POST_PREDICT_MOVIES, {text: data.value.text}).subscribe((res: any)=>{
+        this.reviewSubmitted[index] = data.value.text;
+        this.predictedSentiments[index] = res;
+
+      });
+    }
+  }
+
+  slideChange(event) {
+    this.submitForm.reset();
+  }
+
+  submitFeedback(is_correct: boolean, index) {
+    this.http.post(Endpoints.API_POST_LOG_FEEDBACK,
+     {text: this.reviewSubmitted[index], predicted_sentiment: this.predictedSentiments[index].sentiment, is_correct}).subscribe(
+       res => {
+         this.feedbackLogged[index] = true;
+         this.setRandomColor();
+       }
+     );
+  }
+
+}
